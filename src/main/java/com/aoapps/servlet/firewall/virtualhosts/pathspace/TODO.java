0e1bbb065573ca15819c7b2f445cc9768ec4c181
AO Industries, Inc <support@aoindustries.com>
2022-04-16 01:49:19 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoapps/servlet/firewall/virtualhosts/pathspace/TODO.java
+++ src/main/java/com/aoapps/servlet/firewall/virtualhosts/pathspace/TODO.java
@@ -1,6 +1,6 @@
 /*
  * ao-servlet-firewall-virtual-hosts-path-space - Path space within virtual host support for servlet-based application request filtering.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-servlet-firewall-virtual-hosts-path-space.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.servlet.firewall.virtualhosts.pathspace;
 
 /**
